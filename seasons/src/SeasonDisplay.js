5b5f82df1fe2a4a2d4bf95f7c37eda4be3736067
ESTECHTI <e.s.tech@outlook.com>
2022-02-04 17:14:24 -0300

    Developing functional component

--- seasons/src/SeasonDisplay.js
+++ seasons/src/SeasonDisplay.js
@@ -1,7 +1,37 @@
 import React from 'react';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faSnowflake, faSun } from '@fortawesome/free-solid-svg-icons'
 
-const SeasonDisplay = () => {
-  return <div>Season Display</div>;
+const seasonConfig = {
+  summer: {
+    text: "Lets hit the beach!",
+    iconName: faSun
+  },
+  winter: {
+    text: "Burr it is cold",
+    iconName: faSnowflake
+  }
+}
+
+const getSeason = (lat, month, year) => {
+  if (month > 2 && month < 9) {
+    return lat > 0 ? 'summer' : 'winter';
+  } else {
+    return lat > 0? 'winter' : 'summer';
+  }
+}
+
+const SeasonDisplay = (props) => {
+  const season = getSeason(props.lat, new Date().getMonth());
+  const { text, iconName } = seasonConfig[season];
+
+  return (
+    <div>
+      <FontAwesomeIcon icon={iconName} size="6x"/>
+        <h1>{text}</h1>
+      <FontAwesomeIcon icon={iconName} size="6x"/>
+    </div>
+  )
 }
 
 export default SeasonDisplay;
--- seasons/src/index.js
+++ seasons/src/index.js
@@ -1,8 +1,36 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import SeasonDisplay from './SeasonDisplay';
+class App extends React.Component {
 
-const App = () => {
-  return <div>Hi there!</div>
+  // eslint-disable-next-line no-useless-constructor
+  constructor(props) {
+    super(props);
+
+    // This is the only time we do direct assignment to this.state
+    this.state = {
+      lat: null,
+      errorMessage: ''
+    };
+  }
+  
+  componentDidMount() {
+    window.navigator.geolocation.getCurrentPosition(
+      (position) => this.setState({ lat: position.coords.latitude }),
+      (err) => this.setState({ errorMessage: err.message })
+    );
+  }
+
+  // React says we have to define render!!
+  render () {
+    if (this.state.errorMessage && !this.state.lat) {
+      return <div>Error: { this.state.errorMessage } </div>
+    } else if (!this.state.errorMessage && this.state.lat) {
+      return <SeasonDisplay lat={this.state.lat}/>
+    }
+
+    return <div>Loading!</div>
+  }
 }
 
 ReactDOM.render(
